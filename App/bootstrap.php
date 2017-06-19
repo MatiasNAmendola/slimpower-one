126fce8eff8b8af21142cbdab16f29df6e51602e
Matias Nahuel Améndola <soporte.esolutions@gmail.com>
2017-06-19 11:58:25 -0300

    Update bootstrap.php

--- App/bootstrap.php
+++ App/bootstrap.php
@@ -1,19 +1,7 @@
 <?php
 
-$logDir = dirname(LOG_FILE);
-
-if (!file_exists($logDir)) {
-    $oldmask = umask(0);
-    mkdir($logDir, 0777, TRUE);
-    umask($oldmask);
-}
-
-$logWriter = new \Slim\LogWriter(fopen(LOG_FILE, 'a'));
-
-$app = new \SlimPower\Slim\Slim(array(
-    'mode' => APP_ENV,
-    'log.level' => LOG_LEVEL,
-    'log.writer' => $logWriter));
+$core = \App\Libs\Core::getInstance();
+$app = $core->getSlimInstance();
 
 $authLogin = new SlimPower\Authentication\Callables\DemoAuthenticator($app);
 $authToken = new \SlimPower\Authentication\Callables\TokenNullAuthenticator($app);
@@ -25,16 +13,3 @@ $security->setWarningPaths(unserialize(WARNING_PATH));
 $security->setTokenSecret(TOKEN_SECRET);
 $security->setTokenValidity(TOKEN_VALIDITY);
 $security->start();
-
-$app->hook('slim.after', function () use ($app) {
-    $request = $app->request;
-    $response = $app->response;
-
-    $app->log->debug('');
-    $app->log->debug('------------- ' . date('Y-m-d H:i:s') . ' -------------');
-    $app->log->debug('Request path: ' . $request->getPathInfo());
-    $app->log->debug('Response status: ' . $response->getStatus());
-    $app->log->debug('Method: ' . $request->getMethod());
-    $app->log->debug('IP: ' . $request->getIp());
-});
-
